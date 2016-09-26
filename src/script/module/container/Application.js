42d30039899893ea8154f3efb0e4733eedc7dfd3
Wreulicke <masaya2dies@gmail.com>
2016-09-26 21:25:42 +0900

    add mockable container

--- src/script/module/container/Application.js
+++ src/script/module/container/Application.js
@@ -1,4 +1,5 @@
-import Container from './Container';
-const {container, Component, ClassComponent} = new Container();
+import Container from 'mockable-container';
+const container = new Container();
+const {Component, ClassComponent, } = container.getExportModule();
 export default Component;
 export { container, Component, ClassComponent };
--- src/script/module/container/ComponentRegistrant.js
+++
@@ -1,6 +0,0 @@
-const ComponentRegistrant = (container) => (key) => (target) => {
-  const record = container.regist(key, target);
-  return (...args) => (container.get('Container')().get(record))(...args);
-};
-
-export default ComponentRegistrant;
